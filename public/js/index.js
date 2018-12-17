b596b4b4a0b9e329c3dc2ce570eece92a20f6e80
Ayush <ayushkgupta007@gmail.com>
2018-12-17 21:08:18 +0530

    Add form and show incoming messages

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+var socket = io();
+socket.on('connect',function(){
+    console.log('Connected to server');
+});
+socket.on('disconnect',function(){
+    console.log('server down');
+});
+socket.on('newMessage',function(message){
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+});
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from: 'You',
+        text: jQuery('[name=message]').val()
+    }, function(){
+
+    });
+});
--- server/server.js
+++ server/server.js
@@ -1,9 +1,34 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+var {generateMessage} = require('./utils/message.js');
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
-app.listen(port,() => {
+
+io.on('connection', function(socket){
+    console.log('New user connected');
+
+    socket.on('disconnect', function(){
+        console.log('client down');
+    });
+    socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
+    socket.on('createMessage',(message,callback) => {
+        io.emit('newMessage',generateMessage(message.from,message.text));
+        callback('This is from the server');
+        // socket.broadcast.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+    });
+});
+server.listen(port,() => {
     console.log(`Server on ${port}`);
 })
